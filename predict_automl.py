d33d8de2f6d3f95e28972c69377777d499c58cd0
nshreyasvi <shreyasvi.natraj@etu.unige.ch>
2024-09-19 19:27:48 +0200

    v1.0

--- predict_automl.py
+++ predict_automl.py
@@ -3,6 +3,7 @@ from sklearn.datasets import fetch_openml
 from supervised.automl import AutoML
 from sklearn.metrics import accuracy_score
 import pandas as pd
+import scikitplot as skplt
 
 df_test = pd.read_csv('Test_20/test_set.csv')
 
@@ -12,4 +13,5 @@ y_test = df_test[df_test.columns[0]]
 automl = AutoML(results_path="AutoML_compete_logloss")
 predictions = automl.predict(X_test)
 
+_ = skplt.metrics.plot_confusion_matrix(y_test, predictions, normalize=False)
 print("Test accuracy:", accuracy_score(y_test, predictions.astype(int)))
